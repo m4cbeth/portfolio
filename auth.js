019f943ceb49a71d4087e83d3631fb7ecac3b4d1
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-26 01:54:23 -0700

    init commit; set up next/authjs/prisma

---
+++ auth.js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import Instagram from "next-auth/providers/instagram"
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from '@/lib/prisma'
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub, Google, Instagram],
+  trustHost: true,
+  trustedHosts: [
+    'https://jarenwhitehouse.com',
+    'https://www.jarenwhitehouse.com',
+    'http://localhost:3000'
+  ],
+})
