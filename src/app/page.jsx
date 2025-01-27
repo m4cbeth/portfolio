73757da68b9ce94395fc45a960bf4452b747dace
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-27 13:37:23 -0700

    figured out server functions! see how I redefined!

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,44 @@
 'use server'
 import { signIn, auth } from '../../auth'
 
-async function SignInWithGithub() {
+async function githubSignIn() {
   'use server'
+  await signIn('github')
+}
+
+async function googleSignIn() {
+  'use server'
+  await signIn('google')
+}
+
+function SignInWithGithub() {
   return (
-    <form
-      action={async ()=> {
-        await signIn('github')
-      }}
-    >
+    <form action={githubSignIn}>
       <button type='submit'>Sign in with Github</button>
     </form>
   )
 }
-async function SignInWithGoogle() {
-  'use server'
+
+function SignInWithGoogle() {
   return (
-    <form
-      action={async ()=> {
-        await signIn('google')
-      }}
-    >
+    <form action={googleSignIn}>
       <button type='submit'>Sign in with Google</button>
     </form>
   )
 }
 
 export default async function Home() {
-  
   const session = await auth()
 
   return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
           <pre>{JSON.stringify(session,0,8)}</pre>
           <SignInWithGithub/>
           <SignInWithGoogle/>
-    
-          </div>
-        </main>
-      </div>
-    )
-  }
+        </div>
+      </main>
+    </div>
+  )
+}
