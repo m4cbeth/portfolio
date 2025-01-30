4f20e9c8f742f58f508f9bb377bdac6f948ac14b
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-30 02:34:28 -0700

    homepages looks fine

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,9 @@ import { HeroCopyCopy } from "@/components/made-components/herocopycopy"
 export default async function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+    <div className="">
+      <main className="">
+        <HeroCopyCopy/>
       </main>
     </div>
   )
--- src/components/made-components/herocopycopy.jsx
+++ src/components/made-components/herocopycopy.jsx
@@ -19,8 +19,7 @@ export function HeroCopyCopy() {
           ease: [0.4, 0.0, 0.2, 1],
         }}
         className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto ">
-        With bad copy or code, nothing&apos;s real. Everything is far away. Everything
-        is a{" "}
+        With bad copy or code,<br/>nothing&apos;s real.<br/>Everything is far away.<br/>Everything is a<br/>
         <Highlight className="text-black dark:text-white">
           copy, of a copy, of a copy.
         </Highlight>
