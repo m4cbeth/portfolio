d90587251488e068813fc08e5948972836483d1f
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-02-04 11:59:30 -0700

    finished drinks data

---
+++ src/app/projects/leosapp/drinks-data.jsx
@@ -0,0 +1,114 @@
+const drinks = [
+    {
+        "name": "Leo's Caesar",
+        "glass": "Caesar rimmed Jar",
+        "liquor": ["1oz Smirnoff"],
+        "mix": ["1.5oz Caesar Premix", "7oz Clamato"],
+        "garnish": ["Straw", "Lime", "Pickle Spear"]
+    },
+    {
+        "name": "Buffalo Dill Caesar",
+        "glass": "Buffalo Rimmed Jar",
+        "liquor": "1 oz Dill Vodka",
+        "mix": ["1oz Buffalo Premix", "8oz Clamato"],
+        "garnish": ["Straw", "Lime", "Pickle Spear", "Buff Dill Set"]
+    },
+    {
+        "name": "Shaft",
+        "glass": "DBL Rocks",
+        "liquor": ["0.5oz Smirnoff", "0.5oz Baileys"],
+        "mix": ["Cold Brew"],
+        "garnish": ["Straw"]
+    },
+    {
+        "name": "Lime Margarita",
+        "glass": "DBL Rocks - Half Rim Kosher",
+        "liquor": ["1.5oz Cuervo", "0.5oz Triple Sec"],
+        "mix": ["2oz Lime", "1oz Simple"],
+        "garnish": ["Lime"]
+    },
+    {
+        "name": "Strawberry Peach Margarita",
+        "glass": "DBL Rocks - Half Rim Kosher",
+        "liquor": ["1.5oz Cuervo", "0.5oz Triple Sec"],
+        "mix": ["2oz Strawberry Peach", "1oz Lime"],
+        "garnish": ["Lime"]
+    },
+    {
+        "name": "Spicy Mango Margarita",
+        "glass": "DBL Rocks - Half Rim Spicy",
+        "liquor": ["1.5oz Cuervo", "0.5oz Triple Sec"],
+        "mix": ["2oz Mango Jalapeno", "1oz Lime"],
+        "garnish": ["Lime"]
+    },
+    {
+        "name": "Pineapple Coconut Margarita",
+        "glass": "DBL Rocks - Half Rim Kosher",
+        "liquor": ["1.5oz Cuervo", "0.5oz Triple Sec"],
+        "mix": ["2oz Pineapple Coconut"],
+        "garnish": ["Pineapple Wedge"]
+    },
+    {
+        name: "Red Sangria",
+        glass: "Wine w/ Wheels",
+        liquor: ["4oz Red", "1oz Chambord"],
+        mix: ["1.5oz Orange", "1.5 Pineapple", "1oz Soda"],
+        garnish: ["Straw", "Orange Wheel"]
+    },
+    {
+        name: "White Sangria",
+        glass: "Wine w/ Wheels",
+        liquor: ["4oz White", "1oz Grad Marnier"],
+        mix: ["1.5 Orange", "1.5 Mango"],
+        garnish: ["Straw", "Orange Wheel"]
+    },
+    {
+        name: "Mimosa",
+        glass: "Wine Glass",
+        liquor: ["6oz Sparkling"],
+        mix: ["3oz Juice"],
+        garnish: ["None"]
+    },
+    {
+        name: "Leo's Long Island",
+        glass: "Jar",
+        liquor: ["1oz Long Island"],
+        mix: ["7oz Coke", "2oz GunLime™"],
+        garnish: ["Lemon Wheel"]
+    },
+    {
+        name: "Cactus Water",
+        glass: "Jar",
+        liquor: ["1oz Cuervo"],
+        mix: ["1oz Pear", "0.5oz Lime", "7oz Soda"],
+        garnish: ["Straw", "Lime Wheel"]
+    },
+    {
+        name: "Kentucky Tea",
+        glass: "Jar",
+        liquor: ["0.5oz Bulleit", "0.5oz Peach"],
+        mix: ["1.5oz Peach", "3 dashes bitters", "6oz Soda"],
+        garnish: ["Straw", "Lemon Wheel"]
+    },
+    {
+        name: "Mango Jalapeno Mule",
+        glass: "Jar",
+        liquor: ["1oz Smirnoff"],
+        mix: ["1oz Mango Jalepeno Premix", "6oz Ginger Beer", "0.5oz Lime"],
+        garnish: ["Straw", "Lime"]
+    },
+    {
+        name: "Strawberry Peach Smash",
+        glass: "Jar",
+        liquor: ["1oz Gordon's"],
+        mix: ["2oz Strawberry Peach Premix", "6oz Soda"],
+        garnish: ["Straw", "Lime Wheel"]
+    },
+    {
+        name: "Blackberry Jam Jar",
+        glass: "Jar",
+        liquor: ["1oz Smirnoff"],
+        mix: ["1oz Blackberry", "4oz Lemonade", "3oz Soda"],
+        garnish: ["Straw", "Lemon Wheel"]
+    },
+]
