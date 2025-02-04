54777bf276f5e208daee8b272c6b2ff188deb745
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-02-04 07:39:57 -0700

    starting leos data file

---
+++ src/app/projects/leosapp/layout.jsx
@@ -0,0 +1,13 @@
+
+
+export default async function RootLeosLayout({children}) {
+    return (
+        <html>
+            <head>                
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ src/app/projects/leosapp/page.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default async function LeosApp () {
+    return (
+        <main>
+            <h1>
+                hello
+            </h1>
+            <p>
+                Leo's app
+            </p>
+        </main>
+    )
+}
