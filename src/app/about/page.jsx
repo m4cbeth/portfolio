e9ebf350bd9adf760db68bbf46a5b43774a3e17c
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-08-02 03:37:46 -0600

    working on hiragana page, started with some changes on about me

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,15 +3,17 @@ import { TechAccordian, PlatformAccordian } from "@/components/made-components/p
 export default function About() {
     return (               
         <div className="mx-5">
-            <title>About Jaren</title>
-            <h1 className="text-5xl font-black pt-3">
-                about me
-            </h1>
-            <section>
+            <div className="">
+                <title>About Jaren</title>
+                <h1 className="text-5xl font-black pt-3">
+                    about me
+                </h1>
                 <h2 className=" font-thin text-3xl pb-5">
                     My Current Strong Suites
                 </h2>
-                <div className="flex flex-col md:flex-row md:justify-center gap-10 mx-10">
+            </div>
+            <section>
+                <div className="flex flex-col md:justify-center gap-10 mx-5">
                     <div className="flex-1">
                         <h2 className="text-center font-thin text-2xl pb-5">
                             Tech Experience
