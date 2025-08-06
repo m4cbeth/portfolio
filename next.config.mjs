0287655230305daa92f031bb4eca8794b1e29969
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-08-06 17:01:07 -0600

    installed next mdx and changed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // configure page extentions for MDX
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    // any other config for next goes here
+};
+
+const withMDX = createMDX({
+    // add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next config
+
+export default withMDX(nextConfig);
